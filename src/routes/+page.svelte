fdbd11de990b1ec41da1f5b2673f8233cbcc2a00
jpmiller <legendzing@gmail.com>
2023-02-17 02:34:24 -0600

    feat: homepage rough draft

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,37 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div>
+    <h1 class="top-title">Learns and Builder's Society</h1>
+    <img class="jumbotron" src="../src/img/jumbo-screen.png" alt="jumbotron-screen" />
+
+    <h2>Choose the Perfect Coursework for your Professional and Academic Goals</h2>
+    <img class="banner" src="../src/img/students-planning.png" alt="students planning their future" />
+
+    <!-- AI for Learners -->
+    <div class="grid grid-cols-3 gap-4">
+        <div>
+            <h3>LBS is for all Learners</h3>
+            <img src="../src/img/hologram.png" alt="students in class" />
+            <p>Learn with students of similar interests live around the world in virtual spaces</p>
+        </div>
+        <div>
+            <h3>LBS is for Educators</h3>
+            <img src="../src/img/virtual-educator.png" alt="educator in virtual space" />
+            <p>Anyone can submit courses. Community-approved educators and their content are favored by the recommendation engine and paid by economies of scale.</p>
+        </div>
+        <div>
+            <h3>LBS is the New Classroom</h3>
+            <img src="../src/img/future-classroom.png" alt="a classroom of the future" />
+            <p>AI recommends the perfect coursework based on individual interests; Students learn together across multiple regions; Immutable Certifications are soulbound to a decentralized identity.</p>
+        </div>
+    </div>
+    <h2>Join Now</h2>
+    <p>Find the perfect program for you based on your interests today.</p>
+</div>
+
+<style>
+    .top-title { display:flex; text-align: center; position:absolute; background-color: #fff; }
+    .jumbotron { width: 100%; }
+    .banner { width: 100% }
+    .grid {
+        display: grid;
+    }
+</style>
